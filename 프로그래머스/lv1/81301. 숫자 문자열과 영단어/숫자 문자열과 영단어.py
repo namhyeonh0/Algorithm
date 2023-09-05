f865f593c69585856ef07085f0141d609f6e7ac9
namhyeonh0 <143480257+namhyeonh0@users.noreply.github.com>
2023-09-05 16:49:18 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -0,0 +1,13 @@
+def solution(s):
+    answer = ''
+    a = ''
+    num = {'zero' : 0, 'one' : 1, 'two' : 2, 'three' : 3, 'four' : 4, 'five' : 5, 'six' : 6, 'seven' : 7, 'eight' : 8, 'nine' : 9}
+    for i in s:
+        if i.isdigit() == True:
+            answer += i
+        else:
+            a += i
+            if a in num:
+                answer += str(num[a])
+                a = ''
+    return int(answer)
