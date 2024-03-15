bb3ea24a749f7aded748cb1927d0ecf8b3aaa189
namhyeonh0 <143480257+namhyeonh0@users.noreply.github.com>
2024-03-15 10:02:27 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 146.61 ms, Memory: 10.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
@@ -0,0 +1,28 @@
+def solution(files):
+    answer = []
+    
+    
+    for file in files:
+        fileName = []
+        head, number, tail = "", "", ""
+        headLastIndex = 0
+        for i in range(len(file)):
+            if file[i].isdigit() == False and len(number) == 0:
+                head += file[i].lower()
+            elif file[i].isdigit() == False and len(number) != 0:
+                tail += file[i].lower()
+            elif file[i].isdigit() and len(number) < 5 and len(tail) == 0:
+                number += file[i]
+            elif file[i].isdigit() and len(number) >= 5:
+                tail += file[i]
+                
+        answer.append([head,int(number),tail,files.index(file)])
+        answer.sort(key = lambda x: (x[0],x[1]))
+        
+        answerSorted = []
+        for i in range(len(answer)):
+            answerSorted.append(files[answer[i][3]])
+                
+                
+    return answerSorted
+print(solution(["muzi1.txt","Muzi11","MUZI001.txt","muzi1.TXT"]))
