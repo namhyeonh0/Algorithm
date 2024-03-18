2e02b4884cd4b8aadeaf7b9346226c84eb6282aa
namhyeonh0 <143480257+namhyeonh0@users.noreply.github.com>
2024-03-18 09:54:06 +0900

    [level 2] Title: 오픈채팅방, Time: 77.51 ms, Memory: 49.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
@@ -0,0 +1,34 @@
+def solution(record):
+    answer = []
+    userID_index = {}
+    userID_name = {}
+    idx = 0
+    
+    for string in record:
+        stringList = string.split(" ")
+        if stringList[0] == "Enter":
+            if stringList[1] not in userID_index:
+                answer.append(stringList[2] + "님이 들어왔습니다.")
+                userID_index[stringList[1]] = [idx]
+                userID_name[stringList[1]] = stringList[2]
+            else:
+                for i in userID_index[stringList[1]]:
+                    preName1 = answer[i][:answer[i].find("님")]
+                    answer[i] = answer[i].replace(preName1,stringList[2])
+                answer.append(stringList[2] + "님이 들어왔습니다.")
+                userID_index[stringList[1]].append(idx)
+                userID_name[stringList[1]] = stringList[2]
+            idx += 1
+        elif stringList[0] == "Leave":
+            answer.append(userID_name[stringList[1]] + "님이 나갔습니다.")
+            userID_index[stringList[1]].append(idx)
+            idx += 1
+        else:
+            name = stringList[2]
+            for i in userID_index[stringList[1]]:
+                preName2 = answer[i][:answer[i].find("님")]
+                answer[i] = answer[i].replace(preName2,name)
+            userID_name[stringList[1]] = name
+                
+        
+    return answer
