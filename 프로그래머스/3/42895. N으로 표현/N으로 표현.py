4b1131a3323e19b37d692456136618c7d62fd6ed
namhyeonh0 <143480257+namhyeonh0@users.noreply.github.com>
2024-03-20 11:39:26 +0900

    [level 3] Title: N으로 표현, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.py
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.py
@@ -3,45 +3,34 @@ from collections import deque
 def solution(N, number):
     if N == number:
         return 1
-    answer = 0
     count = 1
     Nlist = [N,-N]
-    Ns = [N*11,N*111,N*1111,N*11111,N*111111]
+    Ns = [N*int("1"*i) for i in range(2,7)]
     countlist = deque([])
-    while count <= 8:
+    while count < 8:
         count += 1
         newNlist = []
         for num in Nlist:
-            for i in range(4):
-                if i == 0:
-                    newNlist.append(num+N)
-                elif i == 1:
-                    newNlist.append(num-N)
-                elif i == 2:
-                    newNlist.append(num*N)
-                else:
-                    newNlist.append(num//N)
-        if count >= 4 and count <= 7:
+            cal(num,N,newNlist)
+        if count >= 4:
             for i in range(count-3):
                 for num in countlist[i+1]:
-                    newNlist.append(num+Ns[i])
-                    newNlist.append(num-Ns[i])
-                    newNlist.append(num*Ns[i])
-                    newNlist.append(num//Ns[i])
-        elif count == 8:
-            for i in range(count-3):
-                for num in countlist[i]:
-                    newNlist.append(num+Ns[i])
-                    newNlist.append(num-Ns[i])
-                    newNlist.append(num*Ns[i])
-                    newNlist.append(num//Ns[i])
-
+                    b = Ns[i]
+                    cal(num,b,newNlist)
+        # elif count == 8:
+        #     for i in range(count-3):
+        #         for num in countlist[i]:
+        #             b = Ns[i]
+        #             cal(num,b,newNlist)
         newNlist.append(N*int("1"*count))
         Nlist = list(set(newNlist))
-        if count >=2 and count <= 6:
-            countlist.appendleft(Nlist)
         if number in Nlist:
             return count
+        countlist.appendleft(Nlist)
     return -1
 
-
+def cal(a,b,l):
+    l.append(a+b)
+    l.append(a-b)
+    l.append(a*b)
+    l.append(a//b)
