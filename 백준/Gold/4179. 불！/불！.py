77c545dfa65adf3de47ecb3e2dc8795fc8108a88
namhyeonh0 <143480257+namhyeonh0@users.noreply.github.com>
2024-04-11 14:45:25 +0900

    [Gold IV] Title: 불!, Time: 536 ms, Memory: 170412 KB -BaekjoonHub

---
+++ 백준/Gold/4179. 불！/불！.py
@@ -0,0 +1,60 @@
+from collections import deque
+
+def bfs(personcheck,firecheck,room,w,h):
+    global time
+    escape = False
+    dx = [1,-1,0,0]
+    dy = [0,0,1,-1]
+    while personcheck:
+        time += 1
+        personNowcheck = personcheck.popleft()
+        personNextcheck = []
+        fireNowcheck = firecheck.popleft()
+        fireNextcheck = []
+        for fireNow in fireNowcheck:
+            fireNow_x = fireNow[1]
+            fireNow_y = fireNow[0]
+            for i in range(4):
+                fireNew_x = fireNow_x + dx[i]
+                fireNew_y = fireNow_y + dy[i]
+                if fireNew_x >= 0 and fireNew_x < w and fireNew_y >= 0 and fireNew_y < h:
+                    if room[fireNew_y][fireNew_x] == '.' or room[fireNew_y][fireNew_x] == 'J':
+                        fireNextcheck.append([fireNew_y,fireNew_x])
+                        room[fireNew_y][fireNew_x] = 'F'
+        for personNow in personNowcheck:
+            personNow_x = personNow[1]
+            personNow_y = personNow[0]
+            if personNow_x == 0 or personNow_x == w-1 or personNow_y == 0 or personNow_y == h-1:
+                escape = True
+                return escape
+            for i in range(4):
+                personNew_x = personNow_x + dx[i]
+                personNew_y = personNow_y + dy[i]
+                if personNew_x >= 0 and personNew_x < w and personNew_y >= 0 and personNew_y < h:
+                    if room[personNew_y][personNew_x] == '.':
+                        personNextcheck.append([personNew_y,personNew_x])
+                        room[personNew_y][personNew_x] = 'J'
+        if len(personNextcheck) != 0:
+            personcheck.append(personNextcheck)
+            firecheck.append(fireNextcheck)
+    return escape
+
+h, w = map(int,input().split())
+room = [list(input()) for _ in range(h)]
+time = 0
+person = []
+fire = []
+for i in range(h):
+    for j in range(w):
+        if room[i][j] == 'J':
+            person.append([i,j])
+        elif room[i][j] == 'F':
+            fire.append([i,j])
+personcheck = deque()
+firecheck = deque()
+personcheck.append(person)
+firecheck.append(fire)
+if bfs(personcheck,firecheck,room,w,h) == True:
+    print(time)
+else:
+    print('IMPOSSIBLE')
