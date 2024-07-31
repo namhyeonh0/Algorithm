abc4606da0ffcc9732978a3b18747f2c937edf9a
남현호 <143480257+namhyeonh0@users.noreply.github.com>
2024-07-31 19:21:40 +0900

    [level 1] Title: [1차] 비밀지도, Time: 3.61 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,28 +1,32 @@
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
+        
         String[] answer = new String[n];
         
-        String[] ansAry = new String[n];
-        //String strFormat = String.format("0%dd",n);
-        
-        for(var i = 0; i < n; i++){
-            int ansEle = arr1[i] | arr2[i];
-            String ansStr = Integer.toBinaryString(ansEle);
-            ansAry[i] = ansStr;
+        for (int i = 0; i < n; i++) {
+            String decoding = "";
+            
+            String decimalToBinary1 = Integer.toBinaryString(arr1[i]);
+            String decimalToBinary2 = Integer.toBinaryString(arr2[i]);
+            
+            int plusZero1 = n - decimalToBinary1.length();
+            int plusZero2 = n - decimalToBinary2.length();
+            for (int j = 0; j < plusZero1; j++) decimalToBinary1 = "0" + decimalToBinary1;
+            for (int j = 0; j < plusZero2; j++) decimalToBinary2 = "0" + decimalToBinary2;
+            char[] charArr1 = decimalToBinary1.toCharArray();
+            char[] charArr2 = decimalToBinary2.toCharArray();
+            
+            for (int j = 0; j < n; j++) {
+                if (charArr1[j] == '1' || charArr2[j] == '1') {
+                    decoding += "#";
+                } else {
+                    decoding += " ";
+                }
+            }
+            
+            answer[i] = decoding;
         }
         
-        for(var j = 0; j < n; j++){
-            if(ansAry[j].length() == n){
-                String str1 = ansAry[j].replace('0',' ').replace('1','#');
-                answer[j] = str1;
-            }
-            else{
-                ansAry[j] = "0".repeat(n - ansAry[j].length()) + ansAry[j];
-                System.out.println(ansAry[j]);
-                String str1 = ansAry[j].replace('0',' ').replace('1','#');
-                answer[j] = str1;               
-            }
-        }
         return answer;
-    }      
+    }
 }
