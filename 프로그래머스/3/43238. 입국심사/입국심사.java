3f29ba75365484043a84f02d9d0ef1fa06e05af9
남현호 <143480257+namhyeonh0@users.noreply.github.com>
2024-08-29 22:55:39 +0900

    [level 3] Title: 입국심사, Time: 61.51 ms, Memory: 86.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -0,0 +1,26 @@
+class Solution {
+    public long solution(int n, int[] times) {        
+        long min = 0l;
+        long max = 100000000000000l;
+        long person = 0l;
+        while (min != max) {
+            long mid = (min + max) / 2;
+            person = minute(mid, n, times);
+            if (person < n) {
+                min = (min + max) / 2 + 1;
+            } else {
+                max = (min + max) / 2;
+            }
+        }
+        
+        return min;
+    }
+    
+    static long minute(Long mid, int n, int[] times) {
+        long person = 0l;
+        for (int time : times) {
+            person += mid / time;
+        }
+        return person;
+    }
+}
