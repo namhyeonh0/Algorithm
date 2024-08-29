7d8f1ac7d6069eacc704da88b485b55ece612845
남현호 <143480257+namhyeonh0@users.noreply.github.com>
2024-08-29 23:49:27 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 3900.56 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.py
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.py
@@ -1,14 +1,17 @@
 def solution(m, n, board):
     answer = 0
     boardList = [[char for char in string] for string in board]
+    
     while True:
         check = find4s(boardList)
         answer += len(check[0])
         if len(check[0]) == 0:
             break
+        
     return answer
-    
+
 def find4s(boardList):
+    
     check4s = []
     for i in range(len(boardList)-1):
         for j in range(len(boardList[0])-1):
@@ -18,17 +21,21 @@ def find4s(boardList):
                 check4s.append([i,j+1])
                 check4s.append([i+1,j])
                 check4s.append([i+1,j+1])
+                
     check4s_done = []
     for pair in check4s:
         if pair not in check4s_done:
             check4s_done.append(pair)
+    
     for pair in check4s_done:
         boardList[pair[0]][pair[1]] = " "
+        
     for i in range(len(boardList)-1,0,-1):
         for j in range(len(boardList[0])):
             for count in range(i):
                 if boardList[i][j] == " ":
                     for k in range(i,0,-1):
                         boardList[k][j] = boardList[k-1][j]
-                    boardList[0][j] = " "    
+                    boardList[0][j] = " "
+                    
     return check4s_done, boardList
