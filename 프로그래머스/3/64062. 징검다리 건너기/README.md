add62d10a2385782e006299ec9fa0cab92027b75
남현호 <143480257+namhyeonh0@users.noreply.github.com>
2024-08-30 18:31:51 +0900

    [level 3] Title: 징검다리 건너기, Time: 41.02 ms, Memory: 65.4 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/README.md
+++ 프로그래머스/3/64062. 징검다리 건너기/README.md
@@ -1,10 +1,10 @@
 # [level 3] 징검다리 건너기 - 64062 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64062#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64062) 
 
 ### 성능 요약
 
-메모리: 81.3 MB, 시간: 55.23 ms
+메모리: 65.4 MB, 시간: 41.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 29일 20:50:46
+2024년 08월 30일 18:31:48
 
 ### 문제 설명
 
--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
@@ -2,24 +2,22 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] stones, int k) {
+        int max = 200000000;
         int len = stones.length;
-        // 인덱스 정보만 담는다.
-        Deque<Integer> dq = new ArrayDeque<>();
-        int max = Integer.MAX_VALUE;
-        
-        for(int i = 0; i < len; ++i){
-            // 윈도우에서 벗어난 원소 제거
-            while(!dq.isEmpty() && dq.peekFirst() <= i - k) {
-                dq.pollFirst();
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
+        for (int i = 0; i < len; i++) {
+            if (!deque.isEmpty() && k <= i - deque.peekFirst()) {
+                deque.pollFirst();
             }
-            // 새로운 원소가 기존의 원소들보다 크면, 작은 원소들 제거
-            while(!dq.isEmpty() && stones[dq.peekLast()] < stones[i]) {
-                dq.pollLast();
+            
+            while (!deque.isEmpty() && stones[deque.peekLast()] < stones[i]) {
+                deque.pollLast();
             }
-            dq.addLast(i);
-            // 각 윈도우에서 최대값 중 최소가 마지막으로 건너뛰어 갈 수 있는 사람 수이다.
-            if(i >= k - 1) {
-                max = Math.min(max, stones[dq.peekFirst()]);
+            
+            deque.addLast(i);
+            
+            if (i >= k - 1) {
+                max = Math.min(max, stones[deque.peekFirst()]);
             }
         }
         
