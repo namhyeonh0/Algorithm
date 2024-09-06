ab8e8e953ed746cde83b5a2a89fe6d4b604149bb
남현호 <143480257+namhyeonh0@users.noreply.github.com>
2024-09-06 14:05:45 +0900

    [level 3] Title: 양과 늑대, Time: 4.30 ms, Memory: 81.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    static boolean[] visited;
+    static int answer = 0;
+    public int solution(int[] info, int[][] edges) {
+        visited = new boolean[info.length];
+        
+        visited[0] = true;
+        dfs(1, 0, info, edges);
+        
+        return answer;
+    }
+    
+    static void dfs(int sheep, int wolf, int[] info, int[][] edges) {
+        if (sheep > wolf) answer = Math.max(answer, sheep);
+        else return;
+        
+        for (int[] edge : edges) {
+            if (visited[edge[0]] == true && visited[edge[1]] == false) {
+                visited[edge[1]] = true;
+                if (info[edge[1]] == 0) dfs(sheep + 1, wolf, info, edges);
+                else dfs(sheep, wolf + 1, info, edges);
+                visited[edge[1]] = false;
+            }
+        }
+    }
+    
+    
+}
